ad36d841decbe51450b4dca8756dc2f761ea7f8c
Shreya111111 <shreyanalawade882@gmail.com>
2023-12-24 16:30:47 +0530

    additional changes

--- codescribe/src/App.js
+++ codescribe/src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import Home from "./components/Home";
 import WebEditor from "./components/Frontend";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import PythonEditor from "./components/Pythoneditor"
+import PythonEditor from "./components/Pythoneditor";
+import MarkdownEditor from "./components/MarkdownEditor.jsx";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/web" component={WebEditor} />
+        <Route path="/markdown" component={MarkdownEditor} />
         <Route path="/python" component={PythonEditor} />
        
       </Switch>
---
+++ codescribe/src/components/MarkdownEditor.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Editor from "./markdown/Editor";
+
+function MarkdownEditor() {
+  return (
+    <div>
+      <Editor />
+    </div>
+  );
+}
+
+export default MarkdownEditor;
